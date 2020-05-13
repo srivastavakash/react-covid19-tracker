a5671a5e76cb4ffd05a188176e94f8846082ba9a
Akash Manoj Shrivastav <34399546+srivastavakash@users.noreply.github.com>
2020-05-13 15:46:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Sources from "./data/Sources";
 import { Route } from "react-router-dom";
-import Sources from "./data/About";
+import ReactGA from "react-ga";
 import StatePage from "./containers/StatePage";
 import "./styles.css";
 import "./TableStyle.css";
 
 export default function App() {
+  useEffect(() => {
+    ReactGA.initialize("UA-166351698-1"); //UA-166351698-1
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <Route
-        path="/country/IN/state-UT/:sName"
-        render={props => <StatePage {...props} isAuthed={true} />}
-      />
-    <Route path="/data-sources-api" component={Sources} />
-    <Route path="/about" component={Sources} />
+      <Route path="/data-sources-api" exact component={Sources} />
+      <Route path="/about" component={Sources} />
+      <Route path="/country/IN/state-UT/:sName" component={StatePage} />
       <Footer />
     </div>
-  ); //1800120820050
+  );
 }
